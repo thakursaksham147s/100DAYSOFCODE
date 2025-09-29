4ae24104c85207727efac5077273ebf93dfd1234
Saksham Thakur <154249636+thakursaksham147s@users.noreply.github.com>
2025-09-29 14:08:19 +0530

    Add files via upload

---
+++ Q-70.c
@@ -0,0 +1,41 @@
+///Q70: Rotate an array to the right by k positions.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+4 5 1 2 3
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+    k = k % n;  // In case k is greater than n
+    int *temp = (int *)malloc(k * sizeof(int));
+    for (int i = 0; i < k; i++) {
+        temp[i] = arr[n - k + i];
+    }
+    for (int i = n - 1; i >= k; i--) {
+        arr[i] = arr[i - k];
+    }
+    for (int i = 0; i < k; i++) {
+        arr[i] = temp[i];
+    }
+    free(temp);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
